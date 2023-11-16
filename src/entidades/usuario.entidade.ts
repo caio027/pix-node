58c2093f91b6cabbf51dfc2ef4ba91345fd30195
Caio de Oliveira <133814446+caio027@users.noreply.github.com>
2023-11-16 19:10:56 -0300

    aa

--- src/entidades/usuario.entidade.ts
+++ src/entidades/usuario.entidade.ts
@@ -3,10 +3,7 @@ export class Usuario {
         public id: number;
         public nome: string;
         public senha: string;
-        public email: string;
-
-        
-        
+        public email: string;       
 }
 
 // teste
